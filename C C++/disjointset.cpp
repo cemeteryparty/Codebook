2d59c6d0070376fd2981a1be556b4b6d0aec8210
Cemetery Corvus <42334422+cemeteryparty@users.noreply.github.com>
2020-03-13 12:34:55 +0800

    Update disjointset.cpp

--- C C++/disjointset.cpp
+++ C C++/disjointset.cpp
@@ -1,34 +1,43 @@
 #include <iostream>
+#include <vector>
 
-int group[10001];
-int FindGroup(int key){
-    while(key != group[key])
-        key = group[key];
-    return key;
-}
-void UNION(int key1,int key2){
-    if(key1 < key2)
-        group[key2] = key1;
-    else 
-        group[key1] = key2;
-}
+class DisjointSet{
+public:
+    std::vector<int> group;
+    DisjointSet(){
+        group.push_back(0);
+    }
+    int Search(int key){
+        while(group[key] != key)
+            key = group[key];
+        return key;
+    }
+    void UNION(int key1,int key2){
+        if(key1 < key2)
+            group[key2] = key1;
+        else 
+            group[key1] = key2;
+    }
+};
 int main(int argc, char const *argv[]){
     int n,m,q,a,b;
     scanf("%d%d%d",&n,&m,&q);
+    DisjointSet d;
     for(int gi = 1;gi <= n;gi++)
-        group[gi] = gi;
+        d.group.push_back(gi);
     for(int i = 0;i < m;i++){
         scanf("%d%d",&a,&b);
-        a = FindGroup(a);
-        b = FindGroup(b);
-        UNION(a,b);
+        a = d.Search(a);
+        b = d.Search(b);
+        if(a != b)
+            d.UNION(a,b);
     }
 	for(int i = 0;i < q;i++){
-	    scanf("%d%d",&a,&b);
-        if(FindGroup(a) != FindGroup(b))
+		scanf("%d%d",&a,&b);
+        if(d.Search(a) != d.Search(b))
             printf("Different group\n");
         else
-	    printf("Same group\n");
+			printf("Same group\n");
     }
     return 0;
 }
